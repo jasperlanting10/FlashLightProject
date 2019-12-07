a448f55242f38d00dd39d7131f9c72eb2c03b3ee
jasperlanting10 <jasper.lanting10@gmail.com>
2019-12-07 20:02:01 +0100

    Basic animatie zit er nu in. Ga nu de maximale breedte inbouwen

--- app/components/CustomSlider.js
+++ app/components/CustomSlider.js
@@ -1,78 +1,90 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  Image,
-  Animated,
-  View,
-  Text,
-  TouchableWithoutFeedback,
-  PanResponder,
-  StatusBar,
-  TouchableOpacity,
-  Slider,
-  ImageBackground,
-  Dimensions
-} from "react-native";
-
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    Image,
+    Animated,
+    View,
+    Text,
+    TouchableWithoutFeedback,
+    PanResponder,
+    StatusBar,
+    TouchableOpacity,
+    Slider,
+    ImageBackground,
+    Dimensions,
+} from 'react-native';
 
 export default class CustomSlider extends Component {
-  constructor (props) {
-    super(props)
-    
-    this.state = { 
-      pan : new Animated.ValueXY(), 
-      scale : new Animated.Value(1)
-    }
-  }
-  componentDidMount = () => {
-    this._panResponder = PanResponder.create({
-      onMoveShouldSetResponderCapture : () => true,
-      onMoveShouldSetPanResponderCapture : () => true,
-      onPanResponderGrant : (evt, gestureState) => {
-        //*start moving
-        Animated.spring(
-          this.state.scale,
-          {toValue : 1.3, friction : 3}
-        ).start() 
-      }, 
-	  onPanResponderMove: () => {
-		    	Animated.event([
-			  null,
-		  ])
-	  }
-	  onPanResponderRelease : (evt, gestureState) => {
-    Aninated.spring(this.state.calse, 
-		  {
-			toValue :1,
-			friction : 3
-		  })
-	  }
-    })  
-  }
+    constructor(props) {
+        super(props);
 
-  render () {
-    return (
-      <View style={styles.mainContainer}>
-        <Animated.View style={[styles.draggerView, 
-          {transform : [{ scale : this.state.scale}] }]}
-          {...this._panResponderpanHandlers}
-          ></Animated.View>
-      </View>
-    )
-  }
+        this.state = {
+            pan: new Animated.ValueXY(),
+            scale: new Animated.Value(1),
+        };
+
+        this.panResponder = PanResponder.create({
+            onMoveShouldSetResponderCapture: () => true,
+            onMoveShouldSetPanResponderCapture: () => true,
+            onPanResponderGrant: (evt, gestureState) => {
+                //*start moving
+                this.state.pan.setOffset({
+                    x: this.state.pan.x._value,
+                    y: this.state.pan.y._value,
+                });
+                this.state.pan.setValue({x: 0, y: 0});
+                Animated.spring(this.state.scale, {
+                    toValue: 1.2,
+                    friction: 3,
+                }).start();
+            },
+            onPanResponderMove: Animated.event([
+                null,
+                {dx: this.state.pan.x, dy: this.state.pan.y},
+            ]),
+            onPanResponderRelease: (evt, gestureState) => {
+                console.log('hier binnen?');
+                this.state.pan.flattenOffset();
+                Animated.spring(this.state.scale, {
+                    toValue: 1,
+                    friction: 3,
+                }).start();
+            },
+        });
+    }
+    componentDidMount = () => {};
+
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+                <Animated.View
+                    style={[
+                        styles.draggerView,
+                        {
+                            transform: [
+                                {scale: this.state.scale},
+                                {translateX: this.state.pan.x},
+                            ],
+                        },
+                    ]}
+                    {...this.panResponder.panHandlers}></Animated.View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  mainContainer : {
-    height : 55,
-    width : '80%',
-    backgroundColor : 'green'
-  },
-  draggerView : {
-    height : '100%',
-    backgroundColor : 'steelBlue',
-    width : '15%'
-  }
-}) 
+    mainContainer: {
+        height: 55,
+        width: '80%',
+        backgroundColor: 'green',
+    },
+    draggerView: {
+        position: 'absolute',
+        height: '100%',
+        backgroundColor: 'red',
+        width: '15%',
+    },
+});
